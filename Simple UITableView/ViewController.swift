bd0f8dabc2030270f5b52e89b1460f0a022b61d2
AntiHaus <wonko@env-x-----mkdir-shitshock222-which-bash-c-echo-n.local>
2014-10-06 13:17:53 -0400

    Messed Up
    No change, this time it’s not a blank project.

--- Simple UITableView/ViewController.swift
+++ Simple UITableView/ViewController.swift
@@ -2,24 +2,83 @@
 //  ViewController.swift
 //  Simple UITableView
 //
-//  Created by Wonko The Sane on 9/23/14.
-//  Copyright (c) 2014 AntiHaus. All rights reserved.
+//  Created by AntiHaus on 9/23/14.
+//
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet var tableView: UITableView?
+    
+    let cellIdentifier = "cellIdentifier"
+    
+    // Array of strings for the tableView
+    
+    var tableData = ["zero","one","two","three","four","five","six","seven"]
+    
+    
+        
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Register the UITableViewCell class with the tableView
+        
+        self.tableView?.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
+        
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // Return number of rows in table
+        
+        return tableData.count
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // Create Resusable Cell, get row string from tableData
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier(self.cellIdentifier) as UITableViewCell
+        
+        cell.textLabel?.text = self.tableData[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    
+    // User Selected a row
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        
+        // Do what you want here
+        
+        let selectValue = self.tableData[indexPath.row]
+        
+        println("You selected row \(indexPath.row) and the string is \(selectValue)")
+    }
+    
+    
+    
 }
 
